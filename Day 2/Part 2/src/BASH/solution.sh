8e8c59dbc739ad1a8051d6cf4c9747de842acd18
Arron Chapman <arron@arronchapman.com>
2021-12-05 18:18:35 -0600

    Adding D2/P2 Bash solution

---
+++ Day 2/Part 2/src/BASH/solution.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Notes;
+#   
+#
+# Requirements;
+#     Bash >= 4
+
+#Read in the entire file
+mapfile -t data < ./TheData.txt
+vPos=0;
+hPos=0;
+aim=0;
+#Pre-load the first window from the first 3 entries in the array
+
+#Read each element of the array
+for (( i=0; i<${#data[@]}; i++ )); do
+    IFS=' ' read -ra line <<< ${data[$i]}
+    case ${line[0]:0:1} in
+        f)
+            hPos=$((hPos+${line[1]}));
+            vPos=$((vPos+(${line[1]}*aim)));
+        ;;
+        d)
+            aim=$((aim+${line[1]}));
+        ;;
+        u)
+            aim=$((aim-${line[1]}));
+        ;;
+    esac
+done
+
+echo $((vPos*hPos))
