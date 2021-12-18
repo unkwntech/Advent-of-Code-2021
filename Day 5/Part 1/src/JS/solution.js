7b29f8c477fa3159d6b556d173c9d4de3b6de52e
Arron Chapman <arron@arronchapman.com>
2021-12-18 16:26:30 -0600

    Creating D5

---
+++ Day 5/Part 1/src/JS/solution.js
@@ -0,0 +1,11 @@
+/*
+    Notes;
+    I converted the data into a JSON array to make processing slightly simpler.
+
+    Requirements;
+        Node
+*/
+
+
+const fs = require("fs");
+const data = fs.readFileSync(__dirname + "\\..\\..\\TheData.txt", { encoding: "utf-8" }).split("\n");
