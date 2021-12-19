f8fb4ec23098a2e71f6e7f2c67045908b19577b9
Arron Chapman <arron@arronchapman.com>
2021-12-18 19:46:20 -0600

    D7/P2 Solution

--- Day 7/Part 2/src/JS/solution.js
+++ Day 7/Part 2/src/JS/solution.js
@@ -14,13 +14,12 @@ const data = fs.readFileSync(__dirname + "\\..\\..\\TheData.txt", { encoding: "u
 let fuelUsed = 0;
 let searchRange = data.reduce((a, b) => (a > b) ? a : b);
 let bestFuelUsed = Number.MAX_VALUE, bestFuelPosition;
-let distance;
+
 for(let i = 0; i < searchRange; i++) {
     fuelUsed = 0;
 
     for(let position of data) {
-        distance = Math.abs(position - i);
-        fuelUsed += (1 + distance) * distance / 2;
+        fuelUsed += Math.abs(position - i);
     }
 
     if(fuelUsed < bestFuelUsed) {
@@ -32,3 +31,4 @@ for(let i = 0; i < searchRange; i++) {
 }
 
 console.log(bestFuelUsed);
+//Wrong 462037, too high
