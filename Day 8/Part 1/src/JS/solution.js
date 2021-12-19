6772dd9a5155f69e593ed01dacf853a69f5100f4
Arron Chapman <arron@arronchapman.com>
2021-12-18 22:11:27 -0600

    D8P1 Solution

--- Day 8/Part 1/src/JS/solution.js
+++ Day 8/Part 1/src/JS/solution.js
@@ -8,3 +8,20 @@
 
 const fs = require("fs");
 const data = fs.readFileSync(__dirname + "\\..\\..\\TheData.txt", { encoding: "utf-8" }).split("\n");
+
+let count = 0;
+
+for(let i = 0; i < data.length; i++) {
+    data[i].split("|")[1].split(/\s+/).forEach(e => {
+        if(e.trim().length == 2)
+            count++;
+        if(e.trim().length == 3)
+            count++;
+        if(e.trim().length == 4)
+            count++;
+        if(e.trim().length == 7)
+            count++;
+    });
+}
+
+console.log(count)
