1c95334fe8de12568374f160684f91b4fbc9da01
Arron Chapman <arron@arronchapman.com>
2021-12-18 19:41:15 -0600

    D7P1 Solution

--- Day 7/Part 1/src/JS/solution.js
+++ Day 7/Part 1/src/JS/solution.js
@@ -7,4 +7,28 @@
 */
 
 const fs = require("fs");
-const data = fs.readFileSync(__dirname + "\\..\\..\\TheData.txt", { encoding: "utf-8" }).split("\n");
+const data = fs.readFileSync(__dirname + "\\..\\..\\TheData.txt", { encoding: "utf-8" })
+    .split(",")
+    .map(n => parseInt(n));
+
+let fuelUsed = 0;
+let searchRange = data.reduce((a, b) => (a > b) ? a : b);
+let bestFuelUsed = Number.MAX_VALUE, bestFuelPosition;
+
+for(let i = 0; i < searchRange; i++) {
+    fuelUsed = 0;
+
+    for(let position of data) {
+        fuelUsed += Math.abs(position - i);
+    }
+
+    if(fuelUsed < bestFuelUsed) {
+        bestFuelUsed = fuelUsed;
+        bestFuelPosition = i;
+    }
+
+    console.log(`Position ${i}: ${fuelUsed} fuel.`)
+}
+
+console.log(bestFuelUsed);
+//Wrong 462037, too high
